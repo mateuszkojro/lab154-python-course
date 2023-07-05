34797468633ff7eccffb271bd9cf0ff03dd665f1
Mateusz Kojro <mateuszkojro@outlook.com>
2023-07-05 18:24:49 +0200

    Comment out more advanced tests

--- topic1-basics/ex1_parsing_csv_test.py
+++ topic1-basics/ex1_parsing_csv_test.py
@@ -18,27 +18,27 @@ class TestParseCSV(unittest.TestCase):
                 },
             )
     
-    def test_parse_ex_sample1_full(self):
-        with open(ASSET_DIR + "ex1_sample1.csv", "r") as f:
-            result = parse_csv(f.read())
-            self.assertEqual(result,
-                {
-                    "A": ["aa", "bb", "cc"],
-                    "B": [1, 2, 3],
-                    "C": [1.1, 2.2, 3.3],
-                },
-            )
+    # def test_parse_ex_sample1_full(self):
+    #     with open(ASSET_DIR + "ex1_sample1.csv", "r") as f:
+    #         result = parse_csv(f.read())
+    #         self.assertEqual(result,
+    #             {
+    #                 "A": ["aa", "bb", "cc"],
+    #                 "B": [1, 2, 3],
+    #                 "C": [1.1, 2.2, 3.3],
+    #             },
+    #         )
 
-    def test_parse_ex_sample2(self):
-        with open(ASSET_DIR + "ex1_sample2.csv") as f:
-            self.assertRaises(ValueError, parse_csv, f.read())
+    # def test_parse_ex_sample2(self):
+    #     with open(ASSET_DIR + "ex1_sample2.csv") as f:
+    #         self.assertRaises(ValueError, parse_csv, f.read())
 
-    def test_parse_ex_sample3(self):
-        with open(ASSET_DIR + "ex1_sample3.csv") as f:
-            self.assertEqual(
-                parse_csv_type_checking(f.read()),
-                {"A": ["aa", "1", "cc"], "B": [None, 2, 3], "C": [1.1, None, -3.3]},
-            )
+    # def test_parse_ex_sample3(self):
+    #     with open(ASSET_DIR + "ex1_sample3.csv") as f:
+    #         self.assertEqual(
+    #             parse_csv_type_checking(f.read()),
+    #             {"A": ["aa", "1", "cc"], "B": [None, 2, 3], "C": [1.1, None, -3.3]},
+    #         )
 
 
 if __name__ == "__main__":
